e403aa9a26b6e4072c1e52f87914312f8dc63754
Casey Hill <bignamehere@gmail.com>
2018-02-01 17:52:01 -0600

    added code for settings of the test controllers

--- src/components/SvgKnob/index.js
+++ src/components/SvgKnob/index.js
@@ -4,36 +4,39 @@ import './style.css';
 
 
 function Value(props) {
-    return <div className="value">{props.value}</div>;
+  return <div className = "value" > {
+    props.value
+  } </div>;
 }
 
 class SvgKnob extends Component {
 
-    constructor(props){
-        super(props);
-        this.state = { 
-            value_min: 0,
-            value_max: 5700,
-            initial_value: 500
-        };
-        this.handleChange = this.handleChange.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    handleChange(e){
-        this.setState({
-            value: e.detail
-        });
-    }
+  handleChange(e) {
+    this.setState({
+      value: e.detail
+    });
+  }
 
+  componentWillMount(){
 
-    render() {
-        return (
-            <div className="knob">
-                <Knob onChange={this.handleChange} />
-                <Value value={this.state.value} />
-            </div>
-        );
-    }
+    // need to update to pass in below object from parent
+
+    this.setState({ knobSettings: this.props.knobSettings });
+  }
+
+  render() {
+    return (
+      <div className="knob">
+        <Knob onChange={this.handleChange} settings={this.state.knobSettings} />
+      </div>
+    );
+  }
 }
 
 export default SvgKnob;
