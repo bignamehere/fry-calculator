0760c1929a32bd2bbb020f18c861d2ca9f3cc2ef
Casey Hill <bignamehere@gmail.com>
2018-02-04 02:58:03 -0600

    adding CRA version

---
+++ src/js/components/knob/knob.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import SvgKnob from 'svg-knob';
+
+class Knob extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    if (this.props.onChange) this.props.onChange(e);
+  }
+
+  componentDidMount() {
+    this.k = new SvgKnob(
+      this.dom,
+      this.props.settings
+    );
+    this.dom.addEventListener("change", this.handleChange);
+  }
+
+  // Not really necessary, but will slightly improve the rendering performance.
+  shouldComponentUpdate() {
+    return this.k === null;
+  }
+
+  render() {
+    return (
+      <svg ref = {elem => this.dom = elem} />
+    );
+  }
+}
+
+export default Knob;
