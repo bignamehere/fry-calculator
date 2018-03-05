2a18c22ac41238ffc72e2715301f7cc1563f99f0
Casey Hill <bignamehere@gmail.com>
2018-03-05 00:01:53 -0600

    updates to color/style

--- src/js/components/displays/PaymentsTotal/paymentsTotal.js
+++ src/js/components/displays/PaymentsTotal/paymentsTotal.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 class PaymentsTotal extends Component {
   render() {
     return (
-      <div className="fry-box fry-box-condensed">
+      <div className="fry-box fry-box-condensed fry-box__title-bold">
         <div className="fry-box__content--lg fry-box__centered">${this.props.value}</div>
         <div className="fry-box__content--m fry-box__centered">Montly Payment</div> 
       </div>
