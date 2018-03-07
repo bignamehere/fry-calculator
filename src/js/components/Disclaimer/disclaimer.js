00cb2ae63a329c2568753155fb60abf10d2021ce
Casey Hill <bignamehere@gmail.com>
2018-03-06 23:18:29 -0600

    updates after production deploy

--- src/js/components/Disclaimer/disclaimer.js
+++ src/js/components/Disclaimer/disclaimer.js
@@ -4,10 +4,24 @@ import React, { Component } from 'react';
 import './disclaimer.scss';
 
 class Disclaimer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      
+    };
+  }
+
+  componentWillMount(){
+    let year = new Date().getFullYear();
+    this.setState({
+      theYear: year
+    });
+  }
+
   render() {
     return (
       <div className="disclaimer">
-        <p>* All prices and amounts are for demonstration purposes only. © 2018 Fry Orthodontics Specialists. All rights reserved.</p> 
+        <p>* All prices and amounts are for demonstration purposes only. ©{this.state.theYear} Fry Orthodontics Specialists. All rights reserved.</p> 
       </div>
     );
   }
